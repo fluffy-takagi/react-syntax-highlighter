f6b40aad6d79c49afaf8e62b0bd516ce4f25a6ac
Keita Takagi <takagi@fluffy.co.jp>
2023-01-01 21:42:19 +0900

    Fix calculation of largest line number

--- src/highlight.js
+++ src/highlight.js
@@ -401,12 +401,7 @@ export default function(defaultAstGenerator, defaultStyle) {
     }
 
     // determine largest line number so that we can force minWidth on all linenumber elements
-    let lineCount = codeTree.value.length;
-    if (lineCount === 1 && codeTree.value[0].type === 'text') {
-      // Since codeTree for an unparsable text (e.g. 'a\na\na') is [{ type: 'text', value: 'a\na\na' }]
-      lineCount = codeTree.value[0].value.split('\n').length;
-    }
-    const largestLineNumber = lineCount + startingLineNumber;
+    const largestLineNumber = code.split('\n').length - 1 + startingLineNumber;
 
     const rows = processLines(
       codeTree,
